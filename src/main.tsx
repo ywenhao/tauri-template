9960cdcc0307a3698c0af4dbb7d9567784cc1764
ywenhao <ywenhao2019@qq.com>
2024-04-24 21:14:23 +0800

    chore: css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import 'uno.css'
+import './styles.scss'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
